1abbc3f6ce374beba05440b8ff709703628ec1e4
CapSOSkw <33869007+CapSOSkw@users.noreply.github.com>
2018-01-29 18:01:09 -0500

    Update optimize_route.py

--- optimize_route.py
+++ optimize_route.py
@@ -26,28 +26,4 @@ class Optimize():
         return route_index
 
 
-if __name__ == '__main__':
-    pass
-    # df = pd.read_csv("sim_route.csv")
-    # x, y = df['lat'].values, df['lng'].values
-    # routine = np.array([[i, j] for i, j in zip(x, y)])
-    # # print(routine)
-    # index_list = df['index'].tolist()
-    #
-    # centroid = 3
-    # cluster = Cluster(k=centroid, route_array=routine, index_list=index_list)
-    # # print(cluster.centroid)
-    # # label_list = cluster.labels
-    #
-    # # route_point, route_index = cluster.point_in_cluster
-    #
-    # center_df = pd.DataFrame.from_records(cluster.centroid, columns=['lat', 'lng'])
-    # center_df['label'] = cluster.predict(cluster.centroid)
-    # # print(center_df)
-    #
-    # car_df = pd.read_csv("sim_cars.csv")
-    # car_df['index'] = -car_df['index']
-    # # print(car_df)
-    #
-    # sa = SA_route()
-    # print(sa.SA_car(center_df, car_df))
+
